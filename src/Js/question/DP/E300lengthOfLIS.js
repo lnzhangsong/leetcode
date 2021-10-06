6a037758f4ef641edd2dec183062a9b7aaac2293
lnzhangsong <lnzhangsong@163.com>
2021-10-06 20:28:49 +0800

    新增300, dp

---
+++ src/Js/question/DP/E300lengthOfLIS.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let dp = new Array(nums.length).fill(0);
+  dp[0] = 1;
+  let res = 1;
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = 1;
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    res = Math.max(res, dp[i]);
+  }
+  return res;
+};
+// @lc code=end
